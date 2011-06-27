617a72fee09eb592b207858a927f9a93ac670ea5
eekbix <eekbix@uniquename.org>
2011-06-26 19:55:28 -0700

    Actually deleted old tree this time. Need to get the hang of git.

--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '866b5bc85ae5fcfa2b12132387dbb88a85cb4319be5b0e05e8b95bf3bf8bab8124e81ff547eadc979fc5151eaaa73cb2621aab6ee7619b0f5ae0ee90bbfb0ff4'
