b8eab2ddb0ec295feff5cc2ed1ca4f75297d5554
eekbix <eekbix@uniquename.org>
2011-07-12 20:05:10 -0400

    fixed post.rb, minimum should have been :minimum

--- rails3-getting-started/blog/app/models/post.rb
+++ rails3-getting-started/blog/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
 	validates :name,	:presence => true
 	validates :title,	:presence => true,
-                        :length => { minimum => 5 }
+                        :length => { :minimum => 5 }
 end
