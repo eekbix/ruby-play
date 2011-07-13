3fb7f1701a345684940eb0380d11c6512e8d33c0
eekbix <eekbix@uniquename.org>
2011-07-12 23:37:45 -0400

    Introduce "before filters" to authenticate some actions of Post and Comment controllers. Add authenticate method to base controller, which demands for credentials using HTTP Basic authentication.

--- rails3-getting-started/blog/app/controllers/application_controller.rb
+++ rails3-getting-started/blog/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user, pass|
+      # hardcoded credentials, for now
+      user == 'admin' && pass == 'password'
+    end
+  end
 end
